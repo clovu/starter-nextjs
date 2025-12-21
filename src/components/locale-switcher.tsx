3e23089ef3dbfbaa9d8308dcfe099111c76e4f18
Clover You <hi@clovu.me>
2025-12-21 18:45:24 +0800

    feat: page loading spinner

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { useRef } from 'react'
+import { useRef, useTransition } from 'react'
 import { Button } from './ui/button'
 import { setUserLocale } from '@/services/locale'
 import { useLocale } from 'next-intl'
+import { Spinner } from './ui/spinner'
 
 interface Props {
   locales: string[]
@@ -11,15 +12,23 @@ interface Props {
 export function LocaleSwitcher({ locales }: Readonly<Props>) {
   const locale = useLocale()
   const index = useRef(locales.findIndex(it => it === locale) ?? 0)
+  const [isPending, startTransition] = useTransition()
 
   function switcher() {
-    const locale = locales[index.current++] 
+    startTransition(() => {
+      const locale = locales[index.current++] 
       ?? locales[
         index.current = 0
       ]
     
-    setUserLocale(locale)
+      setUserLocale(locale)
+    })
   }
+
+  if (isPending)
+    return <Button variant="outline">
+      <Spinner />
+    </Button>
 
   return <Button variant="outline" onClick={switcher}>
     <span className="icon-[carbon--language] size-5"/>
